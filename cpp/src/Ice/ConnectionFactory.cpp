1e9b19f5c8ade348de31ea2d0c21be04c3800ba6
Jose <jose@zeroc.com>
2021-12-29 11:11:06 +0100

    fix

--- cpp/src/Ice/ConnectionFactory.cpp
+++ cpp/src/Ice/ConnectionFactory.cpp
@@ -465,7 +465,7 @@ IceInternal::OutgoingConnectionFactory::findConnection(const vector<EndpointIPtr
     {
 #ifdef ICE_CPP11_COMPILER
         auto connection = find(_connectionsByEndpoint, *p,
-                               [](const ConnectionI& conn)
+                               [](const ConnectionIPtr& conn)
                                {
                                    return conn->isActiveOrHolding();
                                });
