ef34a1f3a4c66ae20a9f25c034f9a325b3f39ea4
Jose <jose@zeroc.com>
2025-02-11 10:01:22 +0100

    Checkpoint

--- packaging/dpkg/build-package.sh
+++ packaging/dpkg/build-package.sh
@@ -9,14 +9,19 @@ fi
 
 # Generate the upstream tarball
 echo "Creating tarball for ICE_VERSION=$ICE_VERSION"
-tar -czf /workspace/zeroc-ice_${ICE_VERSION}.orig.tar.gz --exclude=.git -C /workspace/ice .
+cd /workspace/ice
+git archive --format=tar.gz -o /workspace/zeroc-ice_${ICE_VERSION}.orig.tar.gz HEAD
 
 # Create build directory and unpack
 mkdir -p /workspace/build
 cd /workspace/build
 tar xzf ../zeroc-ice_${ICE_VERSION}.orig.tar.gz
 
-# Copy Debian packaging files and build
+# Copy Debian packaging files into the build directory
 cp -rfv /workspace/ice/packaging/dpkg/debian .
+
+# Build the source package
 dpkg-buildpackage -S
+
+# Build the binary packages
 dpkg-buildpackage -b -uc -us
