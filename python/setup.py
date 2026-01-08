be04526c8d62edca0b655d6e71e7a8a695c29383
Jose <jose@zeroc.com>
2026-01-08 15:26:29 +0100

    Windows build fixes

--- python/setup.py
+++ python/setup.py
@@ -16,7 +16,9 @@ mcpp_url = f"https://github.com/zeroc-ice/mcpp/archive/refs/tags/v{mcpp_version}
 mcpp_local_filename = f"dist/mcpp-{mcpp_version}.tar.gz"
 
 bzip2_version = "1.0.6.6"
-bzip2_url = f"https://github.com/zeroc-ice/bzip2/archive/refs/tags/v{bzip2_version}.tar.gz"
+bzip2_url = (
+    f"https://github.com/zeroc-ice/bzip2/archive/refs/tags/v{bzip2_version}.tar.gz"
+)
 bzip2_local_filename = f"dist/bzip2-{bzip2_version}.tar.gz"
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
@@ -43,7 +45,7 @@ ice_cpp_sources = [
 
 # Include directories for the build process
 include_dirs = [
-    "dist/python/modules/IcePy",
+    "dist/ice/python/modules/IcePy/",
     "dist/ice/cpp/include",
     "dist/ice/cpp/include/generated",
     "dist/ice/cpp/src",
@@ -52,7 +54,18 @@ include_dirs = [
 
 # Platform-specific adjustments
 if sys.platform == "win32":
-    ice_cpp_sources.append(f"../cpp/include/generated/{platform}/{configuration}/Ice")
+    ice_cpp_sources.append(
+        f"../cpp/include/generated/cpp98/{platform}/{configuration}/Ice"
+    )
+    ice_cpp_sources.append(
+        f"../cpp/include/generated/cpp98/{platform}/{configuration}/IceSSL"
+    )
+    ice_cpp_sources.append(
+        f"../cpp/include/generated/cpp98/{platform}/{configuration}/IceDiscovery"
+    )
+    ice_cpp_sources.append(
+        f"../cpp/include/generated/cpp98/{platform}/{configuration}/IceLocatorDiscovery"
+    )
     include_dirs.append(f"dist/bzip2-{bzip2_version}")
 else:
     ice_cpp_sources.extend(
@@ -63,19 +76,15 @@ else:
             "../cpp/include/generated/IceLocatorDiscovery",
         ]
     )
-    include_dirs.append("dist/ice/cpp/src/IceDiscovery/generated/")
-    include_dirs.append("dist/ice/cpp/src/IceLocatorDiscovery/generated/")
-    include_dirs.append("dist/ice/python/modules/IcePy/")
 
 # Define macros used during the build process
 # All the /**/ macros are necessary only on Windows
 define_macros = [
-    ("ICE_DISABLE_PRAGMA_COMMENT", None),
-    ("ICE_API", "/**/"),
-    ("ICE_UTIL_API", "/**/"),
-    ("ICE_SSL_API", "/**/"),
-    ("ICE_DISCOVERY_API", "/**/"),
-    ("ICE_LOCATOR_DISCOVERY_API", "/**/"),
+    ("ICE_STATIC_LIBS", None),
+    ("ICE_BUILDING_SRC", None),
+    ("ICE_BUILDING_ICE", None),
+    ("ICE_BUILDING_ICESSL", None),
+    ("ICE_BUILDING_ICE_LOCATOR_DISCOVERY", None),
 ]
 
 # Platform-specific compile and link arguments
@@ -85,11 +94,33 @@ if sys.platform == "darwin":
     libraries = []
     extra_link_args = ["-framework", "Security", "-framework", "CoreFoundation"]
 elif sys.platform == "win32":
-    define_macros.extend([("WIN32_LEAN_AND_MEAN", None), ("_WIN32_WINNT", "0x0601"), ("BZ_EXPORT", None)])
+    define_macros.extend(
+        [("WIN32_LEAN_AND_MEAN", None), ("_WIN32_WINNT", "0x0601"), ("BZ_EXPORT", None)]
+    )
     extra_compile_args = ["/std:c++14", "/EHsc", "/Zi"]
-    extra_compile_args.extend(["/wd4018", "/wd4146", "/wd4244", "/wd4250", "/wd4251", "/wd4267", "/wd4275", "/wd4996"])
+    extra_compile_args.extend(
+        [
+            "/wd4018",
+            "/wd4146",
+            "/wd4244",
+            "/wd4250",
+            "/wd4251",
+            "/wd4267",
+            "/wd4275",
+            "/wd4996",
+        ]
+    )
     extra_link_args = ["/DEBUG:FULL"]
-    libraries = ["dbghelp", "Shlwapi", "rpcrt4", "advapi32", "Iphlpapi", "secur32", "crypt32", "ws2_32"]
+    libraries = [
+        "dbghelp",
+        "Shlwapi",
+        "rpcrt4",
+        "advapi32",
+        "Iphlpapi",
+        "secur32",
+        "crypt32",
+        "ws2_32",
+    ]
 else:
     extra_compile_args = ["-w"]
     cpp_extra_compile_args = ["-std=c++14"]
@@ -105,12 +136,26 @@ def filter_source(filename: str):
         return False
 
     # Bzip2lib sources
-    bzip2sources = ["blocksort.c", "bzlib.c", "compress.c", "crctable.c", "decompress.c", "huffman.c", "randtable.c"]
+    bzip2sources = [
+        "blocksort.c",
+        "bzlib.c",
+        "compress.c",
+        "crctable.c",
+        "decompress.c",
+        "huffman.c",
+        "randtable.c",
+    ]
     if "bzip2-" in filename and os.path.basename(filename) not in bzip2sources:
         return False
 
     if sys.platform == "win32":
-        for exclude in ["SysLoggerI", "OpenSSL", "SecureTransport", "Service", "DLLMain"]:
+        for exclude in [
+            "SysLoggerI",
+            "OpenSSL",
+            "SecureTransport",
+            "Service",
+            "DLLMain",
+        ]:
             if exclude in filename:
                 # Skip SysLoggerI, OpenSSL and SecureTransport on Windows
                 return False
@@ -147,14 +192,21 @@ class CustomBuildExtCommand(_build_ext):
         # Monkey-patch the compiler to add extra compile args for C++ files. This works around errors with Clang and
         # GCC as they don't accept --std=c++XX when compiling C files. The MSVC backend doesn't use _compile.
         def _compile(
-            obj: str, src: str, ext: str, cc_args: list, extra_postargs: list, pp_opts: list
+            obj: str,
+            src: str,
+            ext: str,
+            cc_args: list,
+            extra_postargs: list,
+            pp_opts: list,
         ):
             original_compile(
                 obj,
                 src,
                 ext,
                 cc_args,
-                cpp_extra_compile_args + extra_postargs if src.endswith(".cpp") else extra_postargs,
+                cpp_extra_compile_args + extra_postargs
+                if src.endswith(".cpp")
+                else extra_postargs,
                 pp_opts,
             )
 
@@ -170,7 +222,13 @@ class CustomBuildExtCommand(_build_ext):
 class CustomSdistCommand(_sdist):
     def include_file(self, filename: str):
         filename = os.path.normpath(filename)
-        if pathlib.Path(filename).suffix.lower() not in [".c", ".cpp", ".h", ".ice", ".py"]:
+        if pathlib.Path(filename).suffix.lower() not in [
+            ".c",
+            ".cpp",
+            ".h",
+            ".ice",
+            ".py",
+        ]:
             return False
         return True
 
@@ -193,18 +251,26 @@ class CustomSdistCommand(_sdist):
         if sys.platform == "win32":
             # Build slice2cpp and slice2py required to generate the C++ and Python sources included in the pip source dist
             msbuild_args = f"/p:Configuration={configuration} /p:Platform={platform}"
-            os.system(f"MSBuild /m ../cpp/msbuild/ice.proj {msbuild_args} /t:NuGetRestore")
-            os.system(f"MSBuild /m ../cpp/msbuild/ice.sln {msbuild_args} /t:slice2cpp;slice2py")
+            os.system(
+                f"MSBuild /m ../cpp/msbuild/ice.proj {msbuild_args} /t:NuGetRestore"
+            )
+            os.system(
+                f"MSBuild /m ../cpp/msbuild/ice.sln {msbuild_args} /t:c++98\\slice2cpp;c++98\\slice2py"
+            )
             # Build the SliceCompile target to generate the Ice, IceDiscovery, and IceLocatorDiscovery
             # sources included in the pip source dist
             for project in ["Ice", "IceSSL", "IceDiscovery", "IceLocatorDiscovery"]:
-                project_path = f"../cpp/src/{project}/msbuild/{project}/{project}.vcxproj"
+                project_path = (
+                    f"../cpp/src/{project}/msbuild/{project}/{project}.vcxproj"
+                )
                 os.system(f"MSBuild {project_path} {msbuild_args} /t:SliceCompile")
 
         else:
             cpp_source_dir = os.path.join(script_directory, "..", "cpp")
             cpp_targets = "slice2cpp slice2py generate-srcs"
-            os.system(f"make OPTIMIZE=yes -C {cpp_source_dir} {cpp_targets} {cpp_source_dir}")
+            os.system(
+                f"make OPTIMIZE=yes -C {cpp_source_dir} {cpp_targets} {cpp_source_dir}"
+            )
 
         for source_dir in ice_cpp_sources:
             for root, dirs, files in os.walk(os.path.normpath(source_dir)):
@@ -213,9 +279,15 @@ class CustomSdistCommand(_sdist):
                         if sys.platform == "win32":
                             # On Windows Skip generated files that doesn't correspond to the current platform
                             # and configuration.
-                            if "generated" in root and os.path.join(platform, configuration) not in root:
+                            if (
+                                "generated" in root
+                                and os.path.join(platform, configuration) not in root
+                            ):
                                 continue
-                            elif "msbuild" in root and os.path.join(platform, configuration) not in root:
+                            elif (
+                                "msbuild" in root
+                                and os.path.join(platform, configuration) not in root
+                            ):
                                 continue
 
                         source_file = os.path.join(root, file)
@@ -226,19 +298,51 @@ class CustomSdistCommand(_sdist):
                         #
                         # In practice we use the macOS / Linux layout for all platforms.
                         if root.startswith(
-                            os.path.join("..", "cpp", "include", "generated", platform, configuration, "Ice")
+                            os.path.join(
+                                "..",
+                                "cpp",
+                                "include",
+                                "generated",
+                                "cpp98",
+                                platform,
+                                configuration,
+                            )
                         ):
-                            relative_path = os.path.join("cpp", "include", "generated", "Ice", file)
+                            relative_path = os.path.join(
+                                "cpp",
+                                "include",
+                                "generated",
+                                os.path.basename(root),
+                                file,
+                            )
+                            print(relative_path)
                         else:
-                            for name in ["Ice", "IceSSL", "IceDiscovery", "IceLocatorDiscovery"]:
-                                if root.startswith(os.path.join("..", "cpp", "src", name, "msbuild")):
+                            for name in [
+                                "Ice",
+                                "IceSSL",
+                                "IceDiscovery",
+                                "IceLocatorDiscovery",
+                            ]:
+                                if root.startswith(
+                                    os.path.join("..", "cpp", "src", name, "msbuild")
+                                ):
                                     if file.endswith(".h"):
-                                        relative_path = os.path.join("cpp", "include", "generated", name, file)
+                                        relative_path = os.path.join(
+                                            "cpp",
+                                            "include",
+                                            "generated",
+                                            name,
+                                            file,
+                                        )
                                     else:
-                                        relative_path = os.path.join("cpp", "src", name, "generated", file)
+                                        relative_path = os.path.join(
+                                            "cpp", "src", name, "generated", file
+                                        )
                                     break
 
-                        target_file = os.path.join(script_directory, "dist/ice", relative_path)
+                        target_file = os.path.join(
+                            script_directory, "dist/ice", relative_path
+                        )
                         target_dir = os.path.dirname(target_file)
                         if not os.path.exists(target_dir):
                             os.makedirs(target_dir)
@@ -250,7 +354,9 @@ class CustomSdistCommand(_sdist):
                 if file.endswith(".cpp") or file.endswith(".h"):
                     source_file = os.path.join(root, file)
                     relative_path = os.path.relpath(source_file, ".")
-                    target_file = os.path.join(script_directory, "dist/ice/python", relative_path)
+                    target_file = os.path.join(
+                        script_directory, "dist/ice/python", relative_path
+                    )
                     target_dir = os.path.dirname(target_file)
                     if not os.path.exists(target_dir):
                         os.makedirs(target_dir)
@@ -284,7 +390,9 @@ class CustomSdistCommand(_sdist):
                 if file.endswith(".py"):
                     source_file = os.path.join(root, file)
                     relative_path = os.path.relpath(source_file, "python")
-                    target_file = os.path.join(script_directory, "dist/lib", relative_path)
+                    target_file = os.path.join(
+                        script_directory, "dist/lib", relative_path
+                    )
                     target_dir = os.path.dirname(target_file)
                     if not os.path.exists(target_dir):
                         os.makedirs(target_dir)
